fe75ddc7f080f1efc1bf3c45760b86749a8333c4
Justin Kachele <justin@kachele.com>
2022-10-13 10:50:17 -0400

    Added Java Docs

--- src/main/java/com/jkachele/game/util/GameMath.java
+++ src/main/java/com/jkachele/game/util/GameMath.java
@@ -17,15 +17,18 @@ public enum GameMath {;
     public static final float EPSILON = 0.00000001f;
 
     /**
-     * Rotates a vector around a base point angle degrees
+     * Rotates a vector around a base point
+     * @param vector Point to be rotated
+     * @param base Point to rotate around
+     * @param angle Angle to rotate in degrees
      */
-    public static void rotate(Vector2f vector, Vector2f base, float angleDegrees) {
+    public static void rotate(Vector2f vector, Vector2f base, float angle) {
         // Move vector to origin
         float x = vector.x - base.x;
         float y = vector.y - base.y;
 
-        float cos = (float) Math.cos(Math.toRadians(angleDegrees));
-        float sin = (float) Math.sin(Math.toRadians(angleDegrees));
+        float cos = (float) Math.cos(Math.toRadians(angle));
+        float sin = (float) Math.sin(Math.toRadians(angle));
 
         // rotate vector around origin by angle degrees
         float xPrime = (x * cos) - (y * sin);
@@ -40,23 +43,45 @@ public enum GameMath {;
         vector.y = yPrime;
     }
 
+    /**
+     * Tests equality between two floats with the given epsilon
+     * @param a A floating point value
+     * @param b A floating point value
+     * @param epsilon The epsilon to test for equality
+     * @return Boolean equality
+     */
     public static boolean floatEquality(float a, float b, float epsilon) {
         return Math.abs(a - b) < (epsilon * Math.max(1.0f, Math.max(Math.abs(a), Math.abs(b))));
     }
 
     /**
-     * Compares the the difference between the 2 floats with an addaptive epsilon of 0.00000001f
+     * Tests equality between two floats with a default epsilon of 0.00000001f
+     * @param a A floating point value
+     * @param b A floating point value
+     * @return Boolean equality
      */
     public static boolean floatEquality(float a, float b) {
         return Math.abs(a - b) < (EPSILON * Math.max(1.0f, Math.max(Math.abs(a), Math.abs(b))));
     }
 
+    /**
+     * Tests equality between two Vectors with the given epsilon.
+     * Both vector parameters must be equal for the vector to be equal
+     * @param a A Vector2f value
+     * @param b A Vector2f value
+     * @param epsilon The epsilon to test for equality
+     * @return Boolean equality
+     */
     public static boolean vector2fEquality(Vector2f a, Vector2f b, float epsilon) {
         return floatEquality(a.x, b.x, epsilon) && floatEquality(a.y, b.y, epsilon);
     }
 
     /**
-     * Compares the the difference between the 2 Vector's components with an addaptive epsilon of 0.00000001f
+     * Tests equality between two Vectors with a default epsilon of 0.00000001f.
+     * Both vector parameters must be equal for the vector to be equal
+     * @param a A Vector2f value
+     * @param b A Vector2f value
+     * @return Boolean equality
      */
     public static boolean vector2fEquality(Vector2f a, Vector2f b) {
         return floatEquality(a.x, b.x) && floatEquality(a.y, b.y);
